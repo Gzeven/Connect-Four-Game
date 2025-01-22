2b581d910d38404c8613f2aa14cd8e1e5426953a
Gzeven <gerben1978@hotmail.com>
2025-01-22 19:29:29 +0100

    suspense

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -330,11 +330,15 @@
 //     </GameContainer>
 //   );
 // } 
-
 'use client';
 
+import { Suspense } from 'react';
 import GameClient from './GameClient';
 
 export default function Page() {
-  return <GameClient />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <GameClient />
+    </Suspense>
+  );
 }

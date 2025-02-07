c9c5c6ff32b885200bdf4a83b6755e8d3a872de3
Gzeven <gerben1978@hotmail.com>
2025-02-07 18:08:43 +0100

    title

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,8 +1,9 @@
 export default function Head() {
     return (
       <>
-        <title>Connect Four</title>
-        <meta name="description" content="Play Connect Four" />
+        <title>Frontend Mentor | Connect Four game</title>
+        <meta name="description" content="Be the first player to connect 4 of the same colored discs in a row (either 
+  vertically, horizontally, or diagonally)." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
        
         <link rel="icon" href="/favicon.ico" />
